06263fbf932067c9dbe360916e0d219aaac7a48b
Daniel Usani <daniel.e.usani@gmail.com>
2018-04-28 22:40:40 -0400

    Initial commit

---
+++ app/Http/Controllers/Auth/ApiRegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+use Illuminate\Http\Request;
+
+class ApiRegisterController extends Controller {
+
+    use RegistersUsers;
+
+    protected $redirectTo = '/home';
+
+    public function __construct() {
+        $this->middleware('guest');
+    }
+
+    protected function validator(array $data) {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+    }
+
+    protected function create(Request $request) {
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::middleware('api')->get('/hello_world', function(){
+    return json_encode(['message' => 'hello world']);
+});
+
+Route::group(['middleware' => ['api', 'cors']], function(){
+    Route::post('auth/register', 'Auth\ApiRegisterController@create');
+});
+
